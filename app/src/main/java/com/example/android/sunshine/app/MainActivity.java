20d94af78891b793aa3555ac16a4df4fb205ff1c
CrystalSundara <crystalmv529@yahoo.com>
2015-03-12 19:25:29 -0400

    Lesson 4c complete

--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -14,14 +14,18 @@ import android.view.MenuItem;
 public class MainActivity extends ActionBarActivity {
 
     private final String LOG_TAG = MainActivity.class.getSimpleName();
+    private final String FORECASTFRAGMENT_TAG = "FFTAG";
+
+    private String mLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        mLocation = Utility.getPreferredLocation(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new ForecastFragment())
+                    .add(R.id.container, new ForecastFragment(), FORECASTFRAGMENT_TAG)
                     .commit();
         }
     }
@@ -56,11 +60,7 @@ public class MainActivity extends ActionBarActivity {
     }
 
     private void openPreferredLocationInMap() {
-        SharedPreferences sharedPrefs =
-                PreferenceManager.getDefaultSharedPreferences(this);
-        String location = sharedPrefs.getString(
-                getString(R.string.pref_location_key),
-                getString(R.string.pref_location_default));
+        String location = Utility.getPreferredLocation(this);
 
         // Using the URI scheme for showing a location found on a map. This super-handy
         // intent can is detailed in teh "Common Intents page of Android's developer site;
@@ -75,12 +75,21 @@ public class MainActivity extends ActionBarActivity {
         if (intent.resolveActivity(getPackageManager()) != null) {
             startActivity(intent);
         } else {
-                Log.d(LOG_TAG, "Couldn't call  " + location + ", no receiving apps installed!");
+            Log.d(LOG_TAG, "Couldn't call  " + location + ", no receiving apps installed!");
         }
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-
+        @Override
+        protected void onResume() {
+            super.onResume();
+            String location = Utility.getPreferredLocation( this );
+            // update the location in our second pane using the fragment manager
+            if (location != null && !location.equals(mLocation)) {
+                ForecastFragment ff = (ForecastFragment)getSupportFragmentManager().findFragmentByTag(FORECASTFRAGMENT_TAG);
+                if ( null != ff ) {
+                    ff.onLocationChanged();
+                }
+                mLocation = location;
+            }
+    }
 }
